ed91a824bd8b2cb69107f92e643060c9690cca07
Jun <varr285@gmail.com>
2023-09-01 00:44:08 +0900

    課題を完了させました。

--- java_curriculum/src/basicClass/Animal.java
+++ java_curriculum/src/basicClass/Animal.java
@@ -1,13 +1,22 @@
 package basicClass;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Animal {
-	
-	public static void main(String[] args) {
+
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
-		
+	public static void main(String args[]) {
+		Dog animal = new Dog("犬");
+	
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		Dog animal2 = new Dog(2);
 		
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
-		
+		LocalDateTime nowTime = LocalDateTime.now();
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s");
+		String formatNowTime = dtf.format(nowTime);
+		System.out.println(formatNowTime);
 	}
+	
 }
--- java_curriculum/src/basicClass/Dog.java
+++ java_curriculum/src/basicClass/Dog.java
@@ -2,11 +2,19 @@ package basicClass;
 
 public class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
-	
+	private String aniName;
 	// Q2：フィールドに動物の数の変数を定義してください。
-	
+	private int aniNum;
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
-	
+	Dog(String newAniName) {
+		aniName = newAniName;
+		System.out.println(aniName);
+	}	
 	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+	Dog(int newAniNum) {
+		aniNum = newAniNum;
+		System.out.println(aniNum);
+	}
+	
 	
 }
