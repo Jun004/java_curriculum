a34c38c8e5acca50bde447c467eb438f1820259b
Jun <varr285@gmail.com>
2023-09-05 20:42:52 +0900

    指摘いただいた箇所を修正しました。

--- java_curriculum/src/basicClass/Animal.java
+++ java_curriculum/src/basicClass/Animal.java
@@ -7,10 +7,16 @@ public class Animal {
 
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
 	public static void main(String args[]) {
-		Dog animal = new Dog("犬");
-	
+		Dog animal = new Dog();
+		animal.setName("犬");
+		String dogName = animal.getName();
+		System.out.println(dogName);
+		
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
-		Dog animal2 = new Dog(2);
+		Dog animal2 = new Dog();
+		animal2.setNum(2);
+		int dogNum = animal2.getNum();
+		System.out.println(dogNum);
 		
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
 		LocalDateTime nowTime = LocalDateTime.now();
--- java_curriculum/src/basicClass/Dog.java
+++ java_curriculum/src/basicClass/Dog.java
@@ -3,18 +3,23 @@ package basicClass;
 public class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
 	private String aniName;
+	
 	// Q2：フィールドに動物の数の変数を定義してください。
 	private int aniNum;
+	
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
-	Dog(String newAniName) {
-		aniName = newAniName;
-		System.out.println(aniName);
-	}	
-	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
-	Dog(int newAniNum) {
-		aniNum = newAniNum;
-		System.out.println(aniNum);
+	void setName(String newAniName) {
+		this.aniName = newAniName;
+	}
+	String getName() {
+		return this.aniName;
 	}
 	
-	
+	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+	void setNum(int newAniNum) {
+		this.aniNum = newAniNum;
+	}
+	int getNum() {
+		return this.aniNum;
+	}
 }
